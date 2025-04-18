2f821f45522c893a5cfe6e585dcb7958313c5104
Luciano Sabino <lb255043@gmail.com>
2025-04-18 18:45:33 -0300

    mais uma

--- src/server/providers/horas/CalcularHoras.js
+++ src/server/providers/horas/CalcularHoras.js
@@ -2,17 +2,25 @@ import prisma from "../../database/prisma.js";
 
 export const CalculatHoras = async (id, dataInicio, dataFim) => {
   try {
+    // Cria o filtro base para o userId
     const where = {
-      userId: Number(id),
+      AND: [
+        {
+          userId: Number(id), // Filtro para o userId
+        },
+        // Adiciona o filtro de data apenas se dataInicio e dataFim forem fornecidos
+        dataInicio && dataFim
+          ? {
+              data: {
+                gte: new Date(dataInicio),
+                lte: new Date(dataFim),
+              },
+            }
+          : {},
+      ],
     };
 
-    if (dataInicio && dataFim) {
-      where.data = {
-        gte: new Date(dataInicio),
-        lte: new Date(dataFim),
-      };
-    }
-
+    // Realiza a agregação para somar as horas com base no filtro
     const result = await prisma.horario.aggregate({
       _sum: {
         horas: true,
@@ -20,6 +28,7 @@ export const CalculatHoras = async (id, dataInicio, dataFim) => {
       where,
     });
 
+    // Retorna o resultado se houver soma de horas, caso contrário, retorna um erro
     if (result._sum.horas != null) {
       return result._sum.horas;
     }
