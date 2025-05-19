f315fde2b53483489ebf8b4b4e6119d7ec7a3604
Luciano Sabino <lb255043@gmail.com>
2025-05-19 01:08:04 -0300

    correção de erro

--- src/server/controller/agenda/ListeAgenda.js
+++ src/server/controller/agenda/ListeAgenda.js
@@ -3,7 +3,7 @@ import { AgendaProvider } from "../../providers/agenda/index.js";
 
 export const listeAgenda = async (req, res) => {
   try {
-    const result = await UserProvider.buscarUser();
+    const result = await AgendaProvider.listaAgenda();
     res.status(StatusCodes.OK).send(result);
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
